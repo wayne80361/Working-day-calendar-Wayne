4e8a32d1869582d618c99b3eb7fde38f69bee0b5
Wayne <wayne80361@gmail.com>
2023-09-01 19:09:28 -0400

    add prevent default to save buttons. And add some comments.

--- script.js
+++ script.js
@@ -10,30 +10,6 @@ function updateTimeRightNow() {
   $("#currentDay").text(timeRightNow);
 }
 
-updateTimeRightNow();
-setInterval(updateTimeRightNow, 1000);
-
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
 // function idea/codes from instructor in office hour, and did a little modification
 function timeBoxGenerator() {
   var newTimeBox = [];
@@ -85,6 +61,7 @@ timeBoxGenerator();
 var boxesContainer = document.getElementById("boxes-container");
 
 boxesContainer.addEventListener("click", function (event) {
+  event.preventDefault();
   if (
     event.target.classList.contains("saveBtn") ||
     event.target.classList.contains("fa-save")
@@ -99,12 +76,18 @@ boxesContainer.addEventListener("click", function (event) {
 // load saved events when page load or refresh
 function loadFromLocalStorage() {
   for (var i = 8; i <= 17; i++) {
+    // genereate key pair from event-8 to 17
     var hour = "event-" + i;
+    // get info from each hour
     var textStored = localStorage.getItem(hour);
-
+    // place for the stored infor
     var textDisplay = document.querySelector(`#hour-${i} .description`);
+
     textDisplay.value = textStored;
   }
 }
 
+// INITIALIZATION
 loadFromLocalStorage();
+updateTimeRightNow();
+setInterval(updateTimeRightNow, 1000);
